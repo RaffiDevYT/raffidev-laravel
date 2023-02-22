3a20c9edc509e279bfc8a240f1700c6b94c41205
RaffiDevYT <raffidevyt@gmail.com>
2023-02-22 21:43:02 +0700

    two time use laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 Route::get('/profile', function () {
     return view('profile.profile');
 });
 Route::get('/blog', function () {
     return view('blog');
 });
-Route::get('/blog/admin', function () {
-    return view('blog.admin.index');
-});
+// Route::get('/blog/admin', function () {
+//     return view('blog.admin.index');
+// });
+
+Route::get('register',[UserController::class, 'register'])->name('register');
+Route::post('register',[UserController::class, 'register_action'])->name('register.action');
+Route::get('login',[UserController::class, 'login'])->name('login');
+Route::post('login',[UserController::class, 'login_action'])->name('login.action');
